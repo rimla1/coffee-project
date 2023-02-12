5e4882e87bb8f63f5f60831ab9f1ad8fac5d8d1f
Almir Muminovic <projectalmir@gmail.com>
2023-02-12 17:10:58 +0100

    Creating fake database with entities inside service file

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+    private coffees: Coffee[] = [{
+        id: 1,
+        name: "Grand Kafa",
+        brand: "C-Kafa",
+        flavours: ["Vanilla", "Chocolate", "Banana"]
+    },
+    {
+        id: 2,
+        name: "Domaca Kafa",
+        brand: "Turska",
+        flavours: ["Gorka", "Slatka"]
+    }]
+
+    
+}
