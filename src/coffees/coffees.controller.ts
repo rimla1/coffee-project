fec2ef5b985d5aca97e6c05b1452d2853bc88ace
Almir Muminovic <projectalmir@gmail.com>
2023-03-22 11:11:42 +0100

    auto-transform payloads to DTO

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -40,6 +40,7 @@ export class CoffeesController {
     @Post()
     @HttpCode(HttpStatus.GONE)
     create(@Body() createCoffeeDto: CreateCoffeeDto){
+        console.log(createCoffeeDto instanceof CreateCoffeeDto)
         return this.coffeesService.create(createCoffeeDto)
     }
 
